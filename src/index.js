d21cc06b8ed5f5e557a0ddc00ab24390dad40f85
IceCrid <icecrid@gmail.com>
2022-08-13 02:38:35 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let getConfig = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let joinArrayResult = [];
+    joinArrayResult = bracketsConfig[i].join('');
+    getConfig.push(joinArrayResult)
+  }
+
+  if (str.includes(getConfig)) {
+    let openBracket = [];
+    let closeBracket = [];
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == bracketsConfig[0][0]) {
+        openBracket.push(str[i])
+      } else {
+        closeBracket.push(str[i])
+      }
+    }
+  }
+  
+  return true;
 }
